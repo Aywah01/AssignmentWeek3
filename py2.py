4806e440b7b3381bbde0fc88c85d45114963e5ac
Aywah01 <rujhanmunawwaruzzaman@gmail.com>
2023-03-26 01:57:11 +0900

    Finish

--- py2.py
+++ py2.py
@@ -3,4 +3,8 @@ from time import gmtime, strftime
 s = strftime("%a, %d %b %Y %H:%M:%S",
              gmtime(1627987508.6496193))
 
+j = strftime("%a, %d %b %Y %H:%M:%S",
+             gmtime(891234812.8345))
+
 print(s)
+print(j)
